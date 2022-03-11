ff846359370293d2977e68a76c074bb97fffba56
rpsPRO <rplazassanchez@gmail.com>
2022-03-11 17:49:01 +0100

    Apariencia Galería

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,17 +1,100 @@
-import { IconButton, TextField, Typography } from "@mui/material";
-import React from "react";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import { getImageUrl, getPosts, deletePostById } from "../firebase/firebase";
+import { Box } from "@mui/system";
 
 export const Gallery = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts(async (snapshot) => {
+      const newPostsPromise = snapshot.docs.map(async (doc) => {
+        const imageName = doc.data().image;
+        const url = await getImageUrl(imageName);
+        return { id: doc.id, ...doc.data(), image: url };
+      });
+      const list = await Promise.all(newPostsPromise);
+      setPosts(list);
+    });
+  }, []);
+
   const navigate = useNavigate();
 
   const handleAddNewPost = () => {
     navigate("/nueva");
   };
 
+  const deletePost = (id) => {
+    try {
+      console.log("Borrar post", id);
+      deletePostById(id);
+    } catch (error) {
+      console.log("Error al borrar el post");
+    }
+  };
+
+  const updatePost = (id) => {
+      
+  }
+
   return (
-    <>
+    <Box>
+      <Container sx={{ py: 2 }} maxWidth="md">
+        <Grid container spacing={4}>
+          {posts.map((post) => (
+            <Grid item key={post.id} xs={12} sm={6} md={4}>
+              <Card
+                sx={{ height: "200", display: "flex", flexDirection: "column" }}
+              >
+                <CardMedia
+                  component="img"
+                  image={
+                    post.image && post.image !== ""
+                      ? post.image
+                      : "https://www.iesvirgendelcarmen.com/wp-content/uploads/2021/01/Museo-Ciencias-1-1024x768.jpg"
+                  }
+                  alt={post.name}
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {post.titulo}
+                  </Typography>
+                  <Typography>{post.description}</Typography>
+                </CardContent>
+                <CardActions>
+                  <Box sx={{ flexGrow: 1 }}></Box>
+                  <IconButton 
+                  aria-label="edit"
+                  onClick={() => updatePost(post.id)}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton
+                    aria-label="delete"
+                    onClick={() => deletePost(post.id)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
       <Typography
         variant="h6"
         component="div"
@@ -36,6 +119,6 @@ export const Gallery = () => {
           style={{ backgroundColor: "white", fontSize: 70 }}
         />
       </IconButton>
-    </>
+    </Box>
   );
 };
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,27 @@
 // Import the functions you need from the SDKs you need
-import { getAuth,
-   createUserWithEmailAndPassword,
-    signInWithEmailAndPassword, 
-    signOut, 
-    updateProfile, 
-    sendPasswordResetEmail,
-    signInWithPopup,
-    GoogleAuthProvider,
-    FacebookAuthProvider 
-  } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  sendPasswordResetEmail,
+  signInWithPopup,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+} from "firebase/auth";
+
+import {
+  addDoc,
+  collection,
+  getFirestore,
+  onSnapshot,
+  doc,
+  deleteDoc,
+  getDoc,
+} from "firebase/firestore";
+
+import { deleteObject, getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
@@ -27,80 +40,116 @@ const firebaseConfig = {
   storageBucket: "pinterestreactweb.appspot.com",
   messagingSenderId: "779036945027",
   appId: "1:779036945027:web:e4f1264a145b20f42609b6",
-  measurementId: "G-0L39PZ1LK5"
+  measurementId: "G-0L39PZ1LK5",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export const auth = getAuth();
+ const auth = getAuth();
 
-export const register = (email, password) => {
-  return createUserWithEmailAndPassword (auth, email, password);
-}
+const db = getFirestore();
+const storage = getStorage();
 
-export const updateName = (userData) => {
+const register = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+const updateName = (userData) => {
   return updateProfile(auth.currentUser, userData);
-}
+};
 
-export const login = (email, password) => {
+const login = (email, password) => {
   return signInWithEmailAndPassword(auth, email, password);
-}
+};
 
-export const resetPassword = (email) => {
+const resetPassword = (email) => {
   return sendPasswordResetEmail(auth, email);
-}
+};
 
-export const logout = () => {
+const logout = () => {
   return signOut(auth);
-}
+};
 
-export const singInGoogle = () => {
+const singInGoogle = () => {
   signInWithPopup(auth, providerGoogle)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
-}
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+};
 
-export const singInFacebook = () => {
+const singInFacebook = () => {
   console.log("Sign in Facebook");
   signInWithPopup(auth, providerFacebook)
-  .then((result) => {
-    // The signed-in user info.
-    const user = result.user;
-console.log("Estoy en el then");
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    const credential = FacebookAuthProvider.credentialFromResult(result);
-    const accessToken = credential.accessToken;
-    console.log(accessToken);
-    // ...
-  })
-  .catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage);
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = FacebookAuthProvider.credentialFromError(error);
-    console.log("Estoy en el catch");
-    // ...
-  });
+    .then((result) => {
+      // The signed-in user info.
+      const user = result.user;
+      console.log("Estoy en el then");
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      const credential = FacebookAuthProvider.credentialFromResult(result);
+      const accessToken = credential.accessToken;
+      console.log(accessToken);
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = FacebookAuthProvider.credentialFromError(error);
+      console.log("Estoy en el catch");
+      // ...
+    });
+};
+
+const uploadImage = (image) => {
+  const storage = getStorage();
+  const imageRef = ref(storage, `/images/${image.name}`);
+  return uploadBytes(imageRef, image);
 }
 
+const addPost = (post) => {
+      return addDoc(collection(db, "pinterest"), post);
+}
+
+const getPosts = (listener) => {
+  return onSnapshot(collection(db, "pinterest"), listener);
+};
+
+const getImageUrl = async (imageName) => {
+  try {
+    return await getDownloadURL(ref(storage, "images/" + imageName));
+  } catch (err) {
+    return "Error al intentar descargar la url de la imagen";
+  }
+};
+
+const deletePostById = async (id) => {
+  const postRef = doc(db, "pinterest", id);
+  const postDoc = await getDoc(postRef);
+  const post = postDoc.data();
+  await deleteDoc(postRef);
+  const imageRef = ref(storage, "images/" + post.image);
+  await deleteObject(imageRef);
+};
+
+export {auth, login, updateName, register, logout, resetPassword, uploadImage, addPost, getPosts, getImageUrl, deletePostById, singInGoogle, singInFacebook};
